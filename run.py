f94393aa66d4b82faee5f9fbf64b02a5e2c9358a
susanaviruglio <>
2023-11-04 19:14:13 +0000

    created index, about and contact html using Flask

--- run.py
+++ run.py
@@ -1,16 +1,32 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
+# Flask has rendered the contents of my index.html template.
+
 
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    # instead of doing <h1>"Hello, World</h1>, we can use render_template"
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=False) #change debug=True to False when submit the project.
+        debug=True)  # change debug=True to False when submit the project.
